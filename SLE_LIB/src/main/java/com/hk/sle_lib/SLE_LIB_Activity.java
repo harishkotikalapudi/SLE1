70333509daa1a4eef7f055704068320089225710
Harish <harish.kotikalapudi@gmail.com>
2022-01-16 21:15:52 +0530

    Initial commit

---
+++ SLE_LIB/src/main/java/com/hk/sle_lib/SLE_LIB_Activity.java
@@ -0,0 +1,14 @@
+package com.hk.sle_lib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SLE_LIB_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sle_lib);
+    }
+}
